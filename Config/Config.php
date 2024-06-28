9ec0f2ea659cd5cd02d3101b2c319414b27fa25d
Guino-Elvis <ginoyujra38@gmail.com>
2024-06-27 23:16:02 -0500

    biblioteca

---
+++ Config/Config.php
@@ -0,0 +1,9 @@
+<?php
+const base_url = "http://localhost/biblioteca/";
+const host = "biblioteca-gino.calqrho6uobh.us-east-1.rds.amazonaws.com";
+const user = "admin";
+const pass = "admin123456789";
+const port = "3306";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
